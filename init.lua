44739bc549c4b17781f62827549ca250dd6e0851
FaceDeer <derksenmobile@gmail.com>
2019-12-16 14:58:33 -0700

    rough out some management code. Untested, probably doesn't work yet

--- init.lua
+++ init.lua
@@ -7,10 +7,9 @@ local delay_between_emerge_calls = minetest.settings:get("idle_emerge_delay") or
 local check_for_non_admin_players = minetest.settings:get_bool("idle_emerge_admin_check", true)
 
 -- adapted from https://stackoverflow.com/questions/398299/looping-in-a-spiral with much modification
-local spiral_pos_iterator = function(pos1, pos2, size)
+local spiral_pos_iterator = function(pos1, pos2, size, skip_to_index)
 	local minp = vector.divide({x = math.min(pos1.x, pos2.x), y = math.min(pos1.y, pos2.y), z = math.min(pos1.z, pos2.z)}, size)
 	local maxp = vector.divide({x = math.max(pos1.x, pos2.x), y = math.max(pos1.y, pos2.y), z = math.max(pos1.z, pos2.z)}, size)
-	--minetest.chat_send_all("minp " .. minetest.pos_to_string(minp) .. " maxp " .. minetest.pos_to_string(maxp))
 	local width = maxp.x-minp.x
 	local depth = maxp.z-minp.z
 	local min_y = math.ceil(minp.y)
@@ -25,8 +24,9 @@ local spiral_pos_iterator = function(pos1, pos2, size)
 	local depth_half = depth/2
 	local i = 0
 	local max_iter = math.max(width, depth)^2
+	local max_index = math.floor(width*depth)
 	
-	return function()
+	local iterator = function()
 		if y < max_y then
 			y = y + 1
 			return {x=(x+width_half)*size, y=y*size, z=(z+depth_half)*size}
@@ -49,6 +49,16 @@ local spiral_pos_iterator = function(pos1, pos2, size)
 		end
 		return {x=(x+width_half)*size, y=y*size, z=(z+depth_half)*size}
 	end
+	
+	if skip_to_index then
+		-- Probably not the most efficient thing in the world to just plough through the iterator's outputs
+		-- to catch up to the desired index, but unlikely to come up often and unlikely to have bugs.
+		for i = 1, skip_to_index do
+			iterator()
+		end
+	end
+	
+	return iterator, max_index
 end
 
 --* `minetest.emerge_area(pos1, pos2, [callback], [param])`
@@ -73,12 +83,39 @@ end
 --          nil if the parameter was absent).
 
 
+local get_queue_display_string = function()
+	if #areas_to_emerge == 0 then
+		return "No idle_emerge tasks currently running."
+	end
+	local outstring = {"Queued idle_emerge tasks:"}
+	for i, task in ipairs(areas_to_emerge) do
+		outstring[#outstring+1] = tostring(i) .. ":\t" .. minetest.pos_to_string(task.pos1) .. " "
+			.. minetest.pos_to_string(task.pos2) .. " queued by " .. task.name
+	end
+	return table.concat(outstring, "\n")
+end
+
 -- Parses a "range" string in the format of "here (number)" or
 -- "(x1, y1, z1) (x2, y2, z2)", returning two position vectors
 local function parse_range_str(player_name, str)
 	local p1, p2
 	local args = str:split(" ")
-	if args[1] == "here" then
+	if args[1] == nil then
+		return false, get_queue_display_string()
+	elseif args[1] == "clear" then
+		if args[2] then
+			local index = tonumber(args[2])
+			if index then
+				table.remove(areas_to_emerge, index)
+				return false, "Cleared queued emerge task " .. arg[2]
+			else
+				return false, "Expected an integer index into the queue of tasks"
+			end
+		else
+			areas_to_emerge = {}
+			return false, "Cleared all queued emerge tasks"
+		end
+	elseif args[1] == "here" then
 		p1, p2 = minetest.get_player_radius_area(player_name, tonumber(args[2]))
 		if p1 == nil then
 			return false, "Unable to get player " .. player_name .. " position"
@@ -95,7 +132,7 @@ local function parse_range_str(player_name, str)
 end
 
 minetest.register_chatcommand("idle_emerge", {
-	params = "(here [<radius>]) | (<pos1> <pos2>)",
+	params = "() | (here [<radius>]) | (<pos1> <pos2>) | (\"clear\" [<index>])",
 	description = "Load (or, if nonexistent, generate) map blocks "
 		.. "contained in area pos1 to pos2 (<pos1> and <pos2> must be in parentheses)",
 	privs = {server=true},
@@ -104,7 +141,8 @@ minetest.register_chatcommand("idle_emerge", {
 		if p1 == false then
 			return false, p2
 		end
-		table.insert(areas_to_emerge, {pos1 = p1, pos2 = p2, name = name, iterator = spiral_pos_iterator(p1, p2, mapblock_size)})
+		local iterator, max_index = spiral_pos_iterator(p1, p2, mapblock_size)
+		table.insert(areas_to_emerge, {pos1 = p1, pos2 = p2, name = name, index = 0, max_index = max_index, iterator = iterator})
 	end,
 })
 
@@ -114,7 +152,9 @@ local emerge_callback = function(blockpos, action, calls_remaining, param)
 	emerging = false
 	emerge_delay = delay_between_emerge_calls
 	if param then
-		minetest.chat_send_player(param.name, "emerged " .. minetest.pos_to_string(vector.multiply(blockpos, 16)))
+		param.index = param.index + 1 -- TODO this will be used for saving and reloading in-progress emerges
+		minetest.chat_send_player(param.name, "emerged " .. minetest.pos_to_string(vector.multiply(blockpos, 16)) 
+			.. " " .. param.index .. "/" .. param.max_index)
 	end
 end
 
